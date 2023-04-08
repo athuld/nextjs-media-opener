2c1e334e1add08ec1f1ec9252e254068e64d8f5c
Athul Dinesan <athuldinesan5@gmail.com>
2023-04-08 13:31:30 +0530

    site: setup the initial media opener with nextjs
    Signed-off-by: Athul Dinesan <athuldinesan5@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,500,1,0" />
       <Head />
       <body>
         <Main />
